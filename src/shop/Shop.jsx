755b28106781952532633686059ae4fc94d43e3e
lucky99ethan <ethansawe99@gmail.com>
2025-02-14 12:38:18 +0300

    submit the final

--- src/shop/Shop.jsx
+++ src/shop/Shop.jsx
@@ -1,26 +1,102 @@
-import React from 'react'
-import PageHeader from '../components/PageHeader'
+import React, { useState } from "react";
+import PageHeader from "../components/PageHeader";
+import Data from "../products.json";
+import ProductCards from "./ProductCards";
+import Pagination from "./Pagination";
+import Search from "./Search";
+import ShopCategory from "./ShopCategory";
+
+const showResults = "Showing 01 – 12 of 139 results";
 
 const Shop = () => {
+  const [GridList, setGridList] = useState(true);
+  const [products, setProducts] = useState(Data);
+  console.log(products);
+
+  // Pagination
+  const [currentPage, setCurrentPage] = useState(1);
+  const productsPerPage = 12;
+  const indexOfLastProduct = currentPage * productsPerPage;
+  const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
+  const currentProducts = products.slice(
+    indexOfFirstProduct,
+    indexOfLastProduct
+  );
+
+  // function to change the current page
+  const paginate = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
+  // filter products based on category
+  const [selectedCategory, setSelectedCategory] = useState("All");
+  const menuItems = ["All", ...new Set(Data.map((val) => val.category))];
+
+  const filterItem = (current) => {
+    if (current === "All") {
+      setProducts(Data);
+    } else {
+      const newItem = Data.filter((newVal) => {
+        return newVal.category === current;
+      });
+      setProducts(newItem);
+    }
+    setSelectedCategory(current);
+  };
+
   return (
     <div>
-      <PageHeader title="Our Shop Page" curPage="shop"/>
-
+      <PageHeader title="Our Shop Page" curPage="shop" />
       {/*  Shop page */}
       <div className="shop-page padding-tb">
         <div className="container">
           <div className="row justify-content-center">
-            <div className='col-lg-8 col-12'>
-                left
+            <div className="col-lg-8 col-12">
+              <article>
+                <div className="shop-title d-flex flex-warp justify-content-between">
+                  <p>{showResults}</p>
+                  <div
+                    className={`product-view-mode ${
+                      GridList ? "gridActive" : "listActive"
+                    }`}
+                  >
+                    <a className="grid" onClick={() => setGridList(!GridList)}>
+                      <i className="icofont-ghost"></i>
+                    </a>
+                    <a className="list" onClick={() => setGridList(!GridList)}>
+                      <i className="icofont-listine-dots"></i>
+                    </a>
+                  </div>
                 </div>
-                <div className='col-lg-4 col-12'>
-                right
+                <div>
+                  <ProductCards GridList={GridList} products={currentProducts} />
                 </div>
+                <Pagination
+                  productsPerPage={productsPerPage}
+                  totalProducts={products.length}
+                  paginate={paginate}
+                  activePage={currentPage}
+                />{" "}
+                {/* Add Pagination component */}
+              </article>
+            </div>
+            <div className="col-lg-4 col-12">
+              <aside>
+                <Search products={products} GridList={GridList} />
+                <ShopCategory
+                  filterItem={filterItem}
+                  setItem={setProducts}
+                  menuItems={menuItems}
+                  setProducts={setProducts}
+                  selectedCategory={selectedCategory}
+                />
+              </aside>
+            </div>
+          </div>
+        </div>
       </div>
-        </div>
-        </div>
     </div>
-  )
-}
+  );
+};
 
-export default Shop
+export default Shop;
